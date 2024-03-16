40cd550057a19d79f3f27824988f54335cef3958
André Arruda <andre23arruda@gmail.com>
2024-03-16 19:59:42 -0300

    feat: Init repo

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align="center">
+    <img alt="Relógio Digital" src="./.github/icon.png" width="50px" />
+</h1>
+
+<h2 align="center">
+  Relógio Digital
+</h2>
+
+<p align="center">
+  <a href="#-demo">Demo</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#instalação">Instalação</a>
+</p>
+
+## 😀 Demo
+<a href="https://andrearruda-clock.vercel.app">Dê uma olhada</a>
+- Abra no navegador do celular e instale para usar offline
+<br/>
+<br/>
+<br/>
+
+## 🚀 Tecnologias
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://reactjs.org)
+
+## 💻 Projeto
+**Relógio Digital no navegador**
+
+## Instalação
+### Pré requisitos
+Ter instalado:
+- [Node](https://nodejs.org/en/download/)
+- [Yarn](https://classic.yarnpkg.com/en/docs/install/)
+
+#### No terminal, rodar
+```sh
+# Clonar esse repositório
+git clone https://github.com/andre23arruda/digital-clock
+
+# Instalar os pacotes do projeto
+yarn install
+
+# Rodar
+yarn start
+```
+
+<div align="center">
+    <img alt="Screen 1" title="Screen 1" src=".github/screen_1.png" width="200px" />
+</div>
+<hr>
+<div align="center">
+    <img alt="Screen 2" title="Screen 2" src=".github/screen_2.png" width="400px" />
+</div>
+<hr>
---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react'
+
+import './App.css'
+
+
+const days = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
+const months = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+
+
+function getCurrentTime() {
+	const fullDate = new Date()
+	return {
+		month: fullDate.getMonth(),
+		date: fullDate.getDate(),
+		year: fullDate.getFullYear(),
+		day: fullDate.getDay(),
+		hours: fullDate.getHours(),
+		minutes: fullDate.getMinutes(),
+		seconds: fullDate.getSeconds(),
+	}
+}
+
+
+export default function App() {
+	const [currentTime, setCurrentTime] = useState(getCurrentTime())
+
+	useEffect(() => {
+		setInterval(() => {
+			setCurrentTime(getCurrentTime())
+		}, 1000)
+	}, [])
+
+	return (
+		<main className="clock-display">
+			<div className="date-field">
+				<div className="day-of-week">
+					<p className="day-alpha">{ days[currentTime.day] }</p>
+					<p className="placeholder">mmm</p>
+					<p className="placeholder">ooo</p>
+					<p className="placeholder">nnn</p>
+					<p className="placeholder">ttt</p>
+					<p className="placeholder">uuu</p>
+					<p className="placeholder">eee</p>
+					<p className="placeholder">sss</p>
+					<p className="placeholder">www</p>
+					<p className="placeholder">hhh</p>
+					<p className="placeholder">rrr</p>
+					<p className="placeholder">fff</p>
+					<p className="placeholder">iii</p>
+				</div>
+
+				<div className="date">
+					<p className="date-number">{ currentTime.date.toString().padStart(2, '0')}</p>
+					<p className="placeholder">88</p>
+				</div>
+
+				<div className="month">
+					<p className="month-alpha">{ months[currentTime.month] }</p>
+					<p className="placeholder">mmm</p>
+					<p className="placeholder">ooo</p>
+					<p className="placeholder">nnn</p>
+					<p className="placeholder">ttt</p>
+					<p className="placeholder">uuu</p>
+					<p className="placeholder">eee</p>
+					<p className="placeholder">sss</p>
+					<p className="placeholder">www</p>
+					<p className="placeholder">hhh</p>
+					<p className="placeholder">rrr</p>
+					<p className="placeholder">fff</p>
+					<p className="placeholder">iii</p>
+					<p className="placeholder">ddd</p>
+					<p className="placeholder">aaa</p>
+					<p className="placeholder">yyy</p>
+				</div>
+
+				<div className="year">
+					<p className="year-number">{ currentTime.year }</p>
+					<p className="placeholder">8888</p>
+				</div>
+			</div>
+
+			<div className="clock-field">
+				<div className="numbers">
+					<p className="hours">{ currentTime.hours.toString().padStart(2, '0') }</p>
+					<p className="placeholder">88</p>
+				</div>
+
+				<div className="colon">
+					<p>:</p>
+				</div>
+
+				<div className="numbers">
+					<p className="minutes">{ currentTime.minutes.toString().padStart(2, '0') }</p>
+					<p className="placeholder">88</p>
+				</div>
+
+				<div className="colon">
+					<p>:</p>
+				</div>
+
+				<div className="numbers">
+					<p className="seconds">{ currentTime.seconds.toString().padStart(2, '0') }</p>
+					<p className="placeholder">88</p>
+				</div>
+			</div>
+		</main>
+	)
+}
