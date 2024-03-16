3b361240c2b8a4bf43043f1b27f51695ed542773
André Arruda <andre23arruda@gmail.com>
2024-03-16 20:10:13 -0300

    feat: Add no sleep

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import NoSleep from 'nosleep.js'
 
 import './App.css'
 
@@ -25,6 +26,9 @@ export default function App() {
 	const [currentTime, setCurrentTime] = useState(getCurrentTime())
 
 	useEffect(() => {
+		const noSleep = new NoSleep()
+		noSleep.enable()
+
 		setInterval(() => {
 			setCurrentTime(getCurrentTime())
 		}, 1000)
